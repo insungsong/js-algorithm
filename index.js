9c521aa26c4850eb3660b24b96428ebad07c0528
songcommit <commit@capa.ai>
2021-09-26 18:52:21 +0900

    js splice개념 및 수정

--- index.js
+++ index.js
@@ -267,7 +267,90 @@
 
 //   console.log(answer);
 // }
-
 // arr = [25, 23, 11, 47, 53, 17, 33];
-
 // console.log(solution(3, arr));
+
+//----------------------------------------------------------------------------------------------------------------
+
+//forEach, map, filter, reduce
+
+// let arr = [10, 11, 12, 13, 14, 15];
+//forEach
+// arr.forEach(
+//   function (item, sub) {
+//     console.log(this);
+//   },
+//   [1, 2]
+// );
+
+//map
+// let answer = arr.map(
+//   function (v, i) {
+//     return v * v;
+//   },
+//   [1, 2]
+// );
+// let answer = arr.map((item) => {
+//   return item % 2 === 0;
+// });
+// console.log("answer:", answer);
+
+//filter
+// let answer = arr.filter((item) => {
+//   return item % 2 === 0;
+// });
+// console.log("answer:", answer);
+
+//reduce
+// let answer = arr.reduce((result, item) => {
+//배열을 생성하는것이 아니라 어떤값을 return한다.
+//x는 ,1이라고 선언했으니까 처음에는 1이된다.
+//y는 배열의 첫번재 값이다 여기서는 10
+//그리고 return으로 x+y를 했다. 이것은 x가 가지는 값이되고
+//y에는 두번재 배열값이 오는 방식이다.
+
+//   console.log("result + item: ", result + item);
+
+//   return result + item;
+// });
+// console.log("answer:", answer);
+
+//----------------------------------------------------------------------------------------------------------------
+//8번 문제 - 일곱 난쟁이
+//왕비를 피해 일곱 난쟁이들과 함께 평화롭게 생활하고 있던 백설공주에게 위기가 찾아왔다.
+//일과를 마치고 돌아온 난쟁이가 일곱 명이 아닌 아홉 명이었던 것이다.
+//아홉 명의 난쟁이는 모두 자신이 "백설 공주와 일곱 난쟁이"의 주인공이라고 주장했다.
+//뛰어난 수학적 직관력을 가지고 있던 백설공주는, 다행스럽게도 일곱 난쟁이의 키의 합이 100이 됨을 기억해 냈다.
+//아홉 난쟁이의 키가 주어졌을 때, 백설공주를 도와 일곱 난쟁이를 찾는 프로그램을 작성하시 오.
+// ▣ 입력설명
+// 아홉 개의 줄에 걸쳐 난쟁이들의 키가 주어진다. 주어지는 키는 100을 넘지 않는 자연수이며,
+//아홉 난쟁이의 키는 모두 다르며, 가능한 정답이 여러 가지인 경우에는 아무거나 출력한다.
+// ▣ 출력설명
+// 입력된 순서대로 일곱 난쟁이의 키를 출력한다.
+// ▣ 입력예제 1
+// 20 7 23 19 10 15 25 8 13
+// ▣ 출력예제 1
+// 20 7 23 19 10 8 13
+
+//문제 풀이
+//결국: 배열에 들어온 값이 100이 될때까지만 더해야한다.
+function solution(arr) {
+  let answer = arr;
+  let sum = answer.reduce((result, arrItem) => result + arrItem, 0);
+  console.log("sum: ", sum);
+  for (let i = 0; i < 8; i++) {
+    for (let j = i + 1; j < 9; j++) {
+      if (sum - (answer[i] + answer[j]) === 100) {
+        console.log("answer[i]: ", answer[i]);
+        console.log("answer[j]: ", answer[j]);
+        answer.splice(j, 1);
+        answer.splice(i, 1);
+      }
+    }
+  }
+
+  return answer;
+}
+
+let arr = [20, 7, 23, 19, 10, 15, 25, 8, 13];
+console.log(solution(arr));
